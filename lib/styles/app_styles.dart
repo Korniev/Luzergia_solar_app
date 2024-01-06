f42bff1c258aa4e5589b8ecb25d0d56ac36eab75
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-06 12:01:35 +0100

    updated colors for app

---
+++ lib/styles/app_styles.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppStyles {
+  static const Color pantone1 = Color(0xFF16375B);
+  static const Color pantone2 = Color(0xFF4CBAB2);
+  static const Color pantone3 = Color(0xFFA5D2D5);
+  static const Color pantone4 = Color(0xFF444D57);
+  static const Color pantone5 = Color(0xFFD5D921);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color mindAro = Color(0xFFEDF67D);
+  static const Color mintGreen = Color(0xFFD7FDEC);
+  static const Color celticBlue = Color(0xFF226CE0);
+}
