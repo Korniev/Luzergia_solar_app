e27201ae86155832feb3e4b3c3abe06a64880c39
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-06 18:52:27 +0100

    updated main

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,60 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:luzergia_solar_app/screens/login.dart';
 import 'package:luzergia_solar_app/styles/app_styles.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final savedThemeMode = await AdaptiveTheme.getThemeMode();
+  runApp(MyApp(savedThemeMode: savedThemeMode));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final AdaptiveThemeMode? savedThemeMode;
+
+  const MyApp({Key? key, this.savedThemeMode}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Luzergia solar app',
-      theme: ThemeData(
-          useMaterial3: true,
-          appBarTheme: const AppBarTheme(
-              backgroundColor: AppStyles.white,
-              foregroundColor: AppStyles.white,
-              elevation: 4),
-          scaffoldBackgroundColor: AppStyles.white,
-          textTheme: GoogleFonts.montserratTextTheme(),
-          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-            backgroundColor: AppStyles.pantone2,
-            selectedItemColor: AppStyles.celticBlue,
-            unselectedItemColor: AppStyles.pantone1,
-            showSelectedLabels: true,
-            showUnselectedLabels: false,
-          )),
-      home: const LoginScreen(),
+    return AdaptiveTheme(
+      light: ThemeData(
+        useMaterial3: true,
+        scaffoldBackgroundColor: AppStyles.white,
+        textTheme: GoogleFonts.montserratTextTheme().apply(
+          bodyColor: AppStyles.black,
+          displayColor: AppStyles.black,
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: AppStyles.pantone2,
+          selectedItemColor: AppStyles.mindAro,
+          unselectedItemColor: AppStyles.pantone1,
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
+        ),
+      ),
+      dark: ThemeData(
+        useMaterial3: true,
+        scaffoldBackgroundColor: AppStyles.dark,
+        textTheme: GoogleFonts.montserratTextTheme().apply(
+          bodyColor: AppStyles.white,
+          displayColor: AppStyles.white,
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: AppStyles.black,
+          selectedItemColor: AppStyles.mindAro,
+          unselectedItemColor: AppStyles.white,
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
+        ),
+      ),
+      initial: savedThemeMode ?? AdaptiveThemeMode.light,
+      builder: (theme, darkTheme) => MaterialApp(
+        title: 'Luzergia solar app',
+        theme: theme,
+        darkTheme: darkTheme,
+        home: const LoginScreen(),
+      ),
     );
   }
 }
