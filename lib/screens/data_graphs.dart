ba068991d19f1f3d22ed92fc688c817abbcf895b
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-20 13:12:12 +0100

    updated data_graphs

--- lib/screens/data_graphs.dart
+++ lib/screens/data_graphs.dart
@@ -1,12 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:luzergia_solar_app/widgets/custom_appbar.dart';
 import 'package:luzergia_solar_app/widgets/custom_navigation_bar.dart';
-import 'package:luzergia_solar_app/widgets/pie_chart.dart';
-
-import '../services/get_access_token.dart';
 
 class DataGraphsScreen extends StatefulWidget {
   const DataGraphsScreen({Key? key}) : super(key: key);
@@ -19,57 +13,16 @@ class _DataGraphsScreenState extends State<DataGraphsScreen> {
   final int _selectedIndex = 0;
   final NavigationService _navigationService = NavigationService();
 
-  Future<dynamic> fetchData() async {
-    var apiUrl =
-        'https://api.enphaseenergy.com/api/v2/systems/123/energy_lifetime';
-    var accessToken = await getAccessToken();
-
-    var response = await http.get(
-      Uri.parse(apiUrl),
-      headers: {
-        'Authorization': 'Bearer $accessToken',
-      },
-    );
-
-    if (response.statusCode == 200) {
-      return json.decode(response.body);
-    } else {
-      debugPrint("Error request: ${response.statusCode}");
-      throw Exception('Error request: ${response.statusCode}');
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(),
-      body: SingleChildScrollView(
+      body: const SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(20.0),
+          padding: EdgeInsets.all(20.0),
           child: Column(
             children: [
-              FutureBuilder(
-                future: fetchData(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const CircularProgressIndicator();
-                  } else if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
-                  } else {
-                    var data = snapshot.data as List<dynamic>;
-                    return ListView.builder(
-                      itemCount: data.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text(data[index]['title']),
-                        );
-                      },
-                    );
-                  }
-                },
-              ),
-              const SizedBox(height: 120),
-              const PieChartSample2(),
+              SizedBox(height: 120),
             ],
           ),
         ),
