090d69b12347ac864bdb53f4603494f12384e33c
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-20 20:08:26 +0100

    upd new page Splash()

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:luzergia_solar_app/styles/app_styles.dart';
 
 class Splash extends StatefulWidget {
   const Splash({super.key});
@@ -8,8 +9,37 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
+  String status = "Inicializando la aplicación...";
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('lib/images/LOGOTIPO LUZERGIA1.png',
+                width: 300.0, height: 300.0),
+            const SizedBox(height: 2, width: 2),
+            Text(status, style: AppStyles.mediumText),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> init() async {
+    await Future.delayed(const Duration(microseconds: 500));
+    status = "Cargando 50%";
+    setState(() {});
+    await Future.delayed(const Duration(microseconds: 500));
+    status = "Cargando 100%";
+    setState(() {});
   }
 }
