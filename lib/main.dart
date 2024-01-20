81b1c4acc7e4a717bb855e991d50b9c8a4544892
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-20 21:27:43 +0100

    updated void method

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:luzergia_solar_app/firebase_options.dart';
 import 'package:luzergia_solar_app/screens/splash.dart';
 import 'package:luzergia_solar_app/styles/app_styles.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final savedThemeMode = await AdaptiveTheme.getThemeMode();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp(savedThemeMode: savedThemeMode));
 }
 
