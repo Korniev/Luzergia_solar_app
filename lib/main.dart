8857bfafa01b7b730bed5301980b4fc4307a32a1
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-20 13:04:15 +0100

    moved dark/light themes from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:luzergia_solar_app/screens/login.dart';
 import 'package:luzergia_solar_app/styles/app_styles.dart';
 
@@ -18,36 +17,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AdaptiveTheme(
-      light: ThemeData(
-        useMaterial3: true,
-        scaffoldBackgroundColor: AppStyles.white,
-        textTheme: GoogleFonts.montserratTextTheme().apply(
-          bodyColor: AppStyles.black,
-          displayColor: AppStyles.black,
-        ),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: AppStyles.pantone2,
-          selectedItemColor: AppStyles.mindAro,
-          unselectedItemColor: AppStyles.pantone1,
-          showSelectedLabels: true,
-          showUnselectedLabels: false,
-        ),
-      ),
-      dark: ThemeData(
-        useMaterial3: true,
-        scaffoldBackgroundColor: AppStyles.dark,
-        textTheme: GoogleFonts.montserratTextTheme().apply(
-          bodyColor: AppStyles.white,
-          displayColor: AppStyles.white,
-        ),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: AppStyles.black,
-          selectedItemColor: AppStyles.mindAro,
-          unselectedItemColor: AppStyles.white,
-          showSelectedLabels: true,
-          showUnselectedLabels: false,
-        ),
-      ),
+      light: AppStyles.lightTheme,
+      dark: AppStyles.darkTheme,
       initial: savedThemeMode ?? AdaptiveThemeMode.light,
       builder: (theme, darkTheme) => MaterialApp(
         title: 'Luzergia solar app',
