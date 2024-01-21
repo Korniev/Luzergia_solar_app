d43fb6029f69e93ff8b552d7b88b0f55d0728299
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-21 14:43:49 +0100

    added ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,19 @@ import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:luzergia_solar_app/firebase_options.dart';
+import 'package:luzergia_solar_app/providers/energy_provider.dart';
 import 'package:luzergia_solar_app/screens/splash.dart';
+import 'package:luzergia_solar_app/services/firebase_initializer.dart';
 import 'package:luzergia_solar_app/styles/app_styles.dart';
+import 'package:provider/provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  final firebaseInitializer = FirebaseInitializer();
+  await firebaseInitializer.createRandomDefaultEnergyData();
+
   final savedThemeMode = await AdaptiveTheme.getThemeMode();
   runApp(MyApp(savedThemeMode: savedThemeMode));
 }
@@ -19,15 +26,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AdaptiveTheme(
-      light: AppStyles.lightTheme,
-      dark: AppStyles.darkTheme,
-      initial: savedThemeMode ?? AdaptiveThemeMode.light,
-      builder: (theme, darkTheme) => MaterialApp(
-        title: 'Luzergia solar app',
-        theme: theme,
-        darkTheme: darkTheme,
-        home: const Splash(),
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => EnergyProvider(),
+      child: AdaptiveTheme(
+        light: AppStyles.lightTheme,
+        dark: AppStyles.darkTheme,
+        initial: savedThemeMode ?? AdaptiveThemeMode.light,
+        builder: (theme, darkTheme) => MaterialApp(
+          title: 'Luzergia solar app',
+          theme: theme,
+          darkTheme: darkTheme,
+          home: const Splash(),
+        ),
       ),
     );
   }
