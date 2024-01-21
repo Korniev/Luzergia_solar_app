b2bcbf43251ac0a9cfdabc2f27d08a6d5da488de
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-21 17:37:16 +0100

    upd logos for appbar

--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -1,26 +1,35 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:luzergia_solar_app/styles/app_styles.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final bool isDarkTheme =
+        AdaptiveTheme.of(context).mode == AdaptiveThemeMode.dark;
+
     return AppBar(
+      backgroundColor: isDarkTheme ? AppStyles.dark : AppStyles.white,
       leading: IconButton(
-        icon: const Icon(Icons.arrow_back, color: Colors.white),
+        icon: Icon(Icons.arrow_back,
+            color: isDarkTheme ? AppStyles.white : AppStyles.black),
         onPressed: () {
           Navigator.of(context).pop();
         },
       ),
-      flexibleSpace: Image.asset(
-        AdaptiveTheme.of(context).mode.isDark
-            ? 'lib/images/LOGOTIPO_LUZERGIA_horizontal_dark.jpg'
-            : 'lib/images/LOGOTIPO_LUZERGIA_horizontal.jpg',
-        fit: BoxFit.cover,
-        height: double.infinity,
-        width: double.infinity,
+      titleSpacing: 0,
+      flexibleSpace: Padding(
+        padding: const EdgeInsets.only(top: 20.0),
+        child: Image.asset(
+          isDarkTheme
+              ? 'lib/images/logo_horizontal_dark_theme.png'
+              : 'lib/images/logo_horizontal_light_theme.png',
+          fit: BoxFit.cover,
+        ),
       ),
+      elevation: 0,
     );
   }
 
