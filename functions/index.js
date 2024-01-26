cbeec1ea7f43e8da088a8b1a319db96fd5ab9f3d
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-26 11:10:16 +0100

    updated cloud function

--- functions/index.js
+++ functions/index.js
@@ -5,37 +5,37 @@ admin.initializeApp();
 exports.sendOrderUpdateNotification = functions.region("europe-west3").firestore
     .document("orderUpdates/{docId}")
     .onWrite(async (change, context) => {
-      const data = change.after.exists ? change.after.data() : null;
+      const beforeData = change.before.exists ? change.before.data() : {};
+      const afterData = change.after.exists ? change.after.data() : {};
       let title = "";
       let body = "";
 
-      if (data) {
-        if (data.pedidoRealizado) {
-          title = "Pedido Realizado";
-          body = "Tu pedido ha sido realizado con éxito.";
-        } else if (data.correoConfirmacionEnviado) {
-          title = "Correo de Confirmación Enviado";
-          body = "Se ha enviado un correo de confirmación.";
-        } else if (data.pedidoEnCamino) {
-          title = "Pedido en Camino";
-          body = "Tu pedido está en camino.";
-        } else if (data.pedidoEntregado) {
-          title = "Pedido Entregado";
-          body = "Tu pedido ha sido entregado.";
-        }
+      if (afterData.pedidoRealizado && !beforeData.pedidoRealizado) {
+        title = "Pedido Realizado";
+        body = "Tu pedido ha sido realizado con éxito.";
+      } else if (afterData.correoConfirmacionEnviado && !beforeData
+          .correoConfirmacionEnviado) {
+        title = "Correo de Confirmación Enviado";
+        body = "Se ha enviado un correo de confirmación.";
+      } else if (afterData.pedidoEnCamino && !beforeData.pedidoEnCamino) {
+        title = "Pedido en Camino";
+        body = "Tu pedido está en camino.";
+      } else if (afterData.pedidoEntregado && !beforeData.pedidoEntregado) {
+        title = "Pedido Entregado";
+        body = "Tu pedido ha sido entregado.";
+      }
 
-        if (title && body) {
-          const message = {
-            notification: {title, body},
-            topic: "orderUpdates",
-          };
+      if (title && body) {
+        const message = {
+          notification: {title, body},
+          topic: "orderUpdates",
+        };
 
-          try {
-            const response = await admin.messaging().send(message);
-            console.log("Successfully sent message:", response);
-          } catch (error) {
-            console.error("Error sending message:", error);
-          }
+        try {
+          const response = await admin.messaging().send(message);
+          console.log("Successfully sent message:", response);
+        } catch (error) {
+          console.error("Error sending message:", error);
         }
       }
     });
