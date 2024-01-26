6b51c8fdaef8bef8327aa02f6a6361f2003dca93
Yevhenii Korniev <94477980+Korniev@users.noreply.github.com>
2024-01-26 15:05:32 +0100

    update for foreground notifications

--- lib/providers/updates_provider.dart
+++ lib/providers/updates_provider.dart
@@ -1,25 +1,50 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class UpdateProvider extends ChangeNotifier {
   bool isInitialized = false;
   String? fcmToken;
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
-  UpdateProvider();
+  UpdateProvider() {
+    _initNotifications();
+  }
+
+  Future<void> _initNotifications() async {
+    var initializationSettingsAndroid =
+        const AndroidInitializationSettings('ic_launcher_foreground');
+    var initializationSettings =
+        InitializationSettings(android: initializationSettingsAndroid);
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
 
   Future<void> init() async {
     fcmToken = await FirebaseMessaging.instance.getToken();
     debugPrint(fcmToken);
 
-    FirebaseMessaging.onMessage.listen((event) {
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       debugPrint("Mensaje recibido");
-      debugPrint("${event.data}");
-      if (event.notification != null) {
-        debugPrint("${event.notification!.title}");
+      debugPrint("${message.data}");
+      if (message.notification != null) {
+        debugPrint("${message.notification!.title}");
+        _showNotification(
+            message.notification!.title, message.notification!.body);
       }
     });
 
     FirebaseMessaging.instance.subscribeToTopic('orderUpdates');
-    debugPrint("Subscribed to 'orderUpdates' topic");
+    debugPrint("Esta escrito a 'orderUpdates' topic");
+  }
+
+  Future<void> _showNotification(String? title, String? body) async {
+    var androidPlatformChannelSpecifics = const AndroidNotificationDetails(
+        'your_channel_id', 'your_channel_name',
+        importance: Importance.max, priority: Priority.high, showWhen: false);
+    var platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+        0, title, body, platformChannelSpecifics);
   }
 }
